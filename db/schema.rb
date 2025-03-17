de61156962d2c441e073a42347e1ef12ac77560c
Dhinakaran <dhinaharanmohan79@gmail.com>
2025-03-17 17:07:59 +0530

    Fix #4: Trying to reinstall ActiveAdmin

--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,76 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_12_155653) do
-    create_table "articles", force: :cascade do |t|
-        t.string "title"
-        t.text "content"
-        t.integer "user_id", null: false
-        t.datetime "created_at", null: false
-        t.datetime "updated_at", null: false
-        t.index ["user_id"], name: "index_articles_on_user_id"
-    end
+ActiveRecord::Schema[8.0].define(version: 2025_03_17_051722) do
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.integer "resource_id"
+    t.string "author_type"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
+  end
 
-    create_table "sessions", force: :cascade do |t|
-        t.integer "user_id", null: false
-        t.string "ip_address"
-        t.string "user_agent"
-        t.datetime "created_at", null: false
-        t.datetime "updated_at", null: false
-        t.index ["user_id"], name: "index_sessions_on_user_id"
-    end
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
 
-    create_table "users", force: :cascade do |t|
-        t.string "name"
-        t.string "email"
-        t.datetime "created_at", null: false
-        t.datetime "updated_at", null: false
-        t.string "encrypted_password", default: "", null: false
-        t.string "reset_password_token"
-        t.datetime "reset_password_sent_at"
-        t.datetime "remember_created_at"
-        t.index ["email"], name: "index_users_on_email", unique: true
-        t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    end
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
-    add_foreign_key "articles", "users"
-    add_foreign_key "sessions", "users"
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "views", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_views_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "articles", "users"
+  add_foreign_key "sessions", "users"
 end
