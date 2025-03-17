0b9016e0fd3a8448c8a1f42938a2d36b67405a1d
Dhinakaran <dhinaharanmohan79@gmail.com>
2025-03-17 14:24:00 +0530

    Fix #3: Saving schemas

--- db/schema.rb
+++ db/schema.rb
@@ -11,37 +11,37 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_03_12_155653) do
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_articles_on_user_id"
-  end
+    create_table "articles", force: :cascade do |t|
+        t.string "title"
+        t.text "content"
+        t.integer "user_id", null: false
+        t.datetime "created_at", null: false
+        t.datetime "updated_at", null: false
+        t.index ["user_id"], name: "index_articles_on_user_id"
+    end
 
-  create_table "sessions", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "ip_address"
-    t.string "user_agent"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sessions_on_user_id"
-  end
+    create_table "sessions", force: :cascade do |t|
+        t.integer "user_id", null: false
+        t.string "ip_address"
+        t.string "user_agent"
+        t.datetime "created_at", null: false
+        t.datetime "updated_at", null: false
+        t.index ["user_id"], name: "index_sessions_on_user_id"
+    end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
+    create_table "users", force: :cascade do |t|
+        t.string "name"
+        t.string "email"
+        t.datetime "created_at", null: false
+        t.datetime "updated_at", null: false
+        t.string "encrypted_password", default: "", null: false
+        t.string "reset_password_token"
+        t.datetime "reset_password_sent_at"
+        t.datetime "remember_created_at"
+        t.index ["email"], name: "index_users_on_email", unique: true
+        t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    end
 
-  add_foreign_key "articles", "users"
-  add_foreign_key "sessions", "users"
+    add_foreign_key "articles", "users"
+    add_foreign_key "sessions", "users"
 end
