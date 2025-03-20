c506733b1a848e2dd2720e09bd5429ad2ab531c0
Dhinakaran <dhinaharanmohan79@gmail.com>
2025-03-20 10:55:55 +0530

    Fix #4: Resolving indentation

--- app/models/blog_comment.rb
+++ app/models/blog_comment.rb
@@ -4,7 +4,8 @@ class BlogComment < ApplicationRecord
     def self.ransackable_attributes(auth_object = nil)
         ["id", "id_value", "article_id", "content", "user_id"]
     end
+    
     def self.ransackable_associations(auth_object = nil)
-    ["article", "user"]
-  end
+        ["article", "user"]
+    end
 end
