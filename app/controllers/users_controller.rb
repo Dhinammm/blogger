239d6ea6ee7f8c93ad2b9aeed4d3e8845c8a6a77
Dhinakaran <dhinaharanmohan79@gmail.com>
2025-03-21 11:16:07 +0530

    Fix #1: Changes made as per review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
     def index
-        @user = User.all
+        @users = User.all
     end
 
     def show
         begin
             @user = User.find(params[:id])
-            @article = Article.where("user_id = ?", params[:id])
+            @articles = Article.where("user_id = ?", params[:id])
         rescue
             render file: Rails.public_path.join('404.html'), status: :not_found, layout: true
         end
