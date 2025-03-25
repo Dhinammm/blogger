5f637afc5470d55bd8672db14a35f0739c7e5b64
Dhinakaran <dhinaharanmohan79@gmail.com>
2025-03-25 10:32:24 +0530

    Fix #5: Removing unnecessary files

--- app/controllers/admin_users/omniauth_callbacks_controller.rb
+++ app/controllers/admin_users/omniauth_callbacks_controller.rb
@@ -1,20 +1,18 @@
 class AdminUsers::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-   
-  skip_before_action :verify_authenticity_token, only: [:google_oauth2]
+
+    skip_before_action :verify_authenticity_token, only: [:google_oauth2]
     def google_oauth2
-      puts '333333333333333333333333333333333333333333'
-    @admin_user = AdminUser.from_omniauth(request.env['omniauth.auth'])
-
-    if @admin_user
-      sign_in_and_redirect @admin_user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
-    else
-      redirect_to new_admin_user_session_path, alert: "Access Denied"
+        @admin_user = AdminUser.from_omniauth(request.env['omniauth.auth'])
+        if @admin_user
+            sign_in_and_redirect @admin_user, event: :authentication
+            set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
+        else
+            redirect_to new_admin_user_session_path, alert: "Access Denied"
+        end
     end
-  end
 
-  def failure
-    redirect_to new_admin_user_session_path, alert: "Authentication failed"
-  end
+    def failure
+        redirect_to new_admin_user_session_path, alert: "Authentication failed"
+    end
 end
 
